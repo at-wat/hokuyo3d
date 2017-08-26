489ffc1d78e62c05bcbe9479590e3d81738d2985
Atsushi Watanabe <atsushi.w@ieee.org>
2017-08-26 14:46:13 +0900

    Save cache only on master.

--- .travis.scripts/save_cache.sh
+++ .travis.scripts/save_cache.sh
@@ -3,13 +3,13 @@
 set -o errexit
 set -o verbose
 
-#if [[ ${TRAVIS_BRANCH} == "master" ]] && [[ ${TRAVIS_PULL_REQUEST} == "false" ]];
-#then
+if [[ ${TRAVIS_BRANCH} == "indigo-devel" ]] && [[ ${TRAVIS_PULL_REQUEST} == "false" ]];
+then
 	mkdir -p $(dirname ${DOCKER_CACHE_FILE})
 	docker save $(docker history -q ros-hokuyo3d:latest | grep -v '<missing>') | lz4 -zcf - > ${DOCKER_CACHE_FILE}
 
 	echo "------------"
 	ls -lh $(dirname ${DOCKER_CACHE_FILE})
 	echo "------------"
-#fi
+fi
 
