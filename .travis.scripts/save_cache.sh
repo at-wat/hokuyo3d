e0667e2bd27913c3025633f1d30341a86b51380c
Atsushi Watanabe <atsushi.w@ieee.org>
2017-08-26 14:39:41 +0900

    Use lz4 instead of bz2.

--- .travis.scripts/save_cache.sh
+++ .travis.scripts/save_cache.sh
@@ -6,7 +6,7 @@ set -o verbose
 #if [[ ${TRAVIS_BRANCH} == "master" ]] && [[ ${TRAVIS_PULL_REQUEST} == "false" ]];
 #then
 	mkdir -p $(dirname ${DOCKER_CACHE_FILE})
-	docker save $(docker history -q ros-hokuyo3d:latest | grep -v '<missing>') | bzip2 > ${DOCKER_CACHE_FILE}
+	docker save $(docker history -q ros-hokuyo3d:latest | grep -v '<missing>') | lz4 -zcf - > ${DOCKER_CACHE_FILE}
 
 	echo "------------"
 	ls -lh $(dirname ${DOCKER_CACHE_FILE})
